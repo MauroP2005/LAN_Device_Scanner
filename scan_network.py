fdabc58856e4264d74d42a62e7bc69ab98baac35
Mauro P <maurop2005@outlook.com>
2025-06-06 10:43:26 -0400

    Add threaded scanning with ThreadPoolExecutor for speed

--- scan_network.py
+++ scan_network.py
@@ -1,5 +1,6 @@
 import subprocess
 from network_info import get_local_network_info
+from concurrent.futures import ThreadPoolExecutor
 
 def ping(ip):
     """
@@ -13,11 +14,23 @@ def ping(ip):
     return result.returncode == 0
 
 def scan_subnet(network):
+    """
+    Scans subnet using multiple threads for faster pinging.
+    """
     print(f"Scanning subnet: {network}")
-    for ip in network.hosts():
-        if ping(str(ip)):
+    alive_hosts = []
+    
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        results = executor.map(ping, map(str, network.hosts()))
+        
+    for ip, is_alive in  zip(network.hosts(), results):
+        if is_alive:
             print(f"[*] {ip} is online")
-            
+            alive_hosts.append(str(ip))
+    
+    print(f"\nScan complete. {len(alive_hosts)} devices found!")
+    return alive_hosts
+    
 if __name__ == "__main__":
     _, _, subnet = get_local_network_info()
     scan_subnet(subnet)
